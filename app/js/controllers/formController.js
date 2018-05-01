7bab8c286928a148f10c895c5c524978fef8d6c9
Bradley Samuelson <brad@lineheight.co>
2018-05-01 19:55:30 -0400

    fix broken contact form

--- app/js/controllers/formController.js
+++ app/js/controllers/formController.js
@@ -4,7 +4,7 @@ app.controller('formController', ['$scope', '$http', function($scope, $http) {
   $scope.processForm = function() {
     $http({
       method  : 'POST',
-      url     : 'contact.php',
+      url     : 'app/contact.php',
       data    : $.param($scope.formData),  // pass in data as strings
       headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
     })
